72943844f66dcd815a15d7921cdc79066b580c83
Adrian Jurczak <vksegfault@gmail.com>
2023-01-12 14:09:28 +0100

    init

---
+++ flask-be/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
+
+@app.route('/members')
+def members():
+    res = jsonify({"members": "Adrian"})
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
